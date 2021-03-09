242b5ca8ab9d549e225010a980cfc93e1ee7655b
bas080 <bas080@hotmail.com>
2021-03-10 00:35:22 +0100

    Simplify the toPairs implementation

--- src/index.test.js
+++ src/index.test.js
@@ -48,8 +48,7 @@ test('Does not match on array that is too short', t => {
 test('Implement toPairs function using patroon', t => {
   const toPairs = patroon(
     [_, _], ([a, b, ...c], p=[]) => toPairs(c, [...p , [a, b]]),
-    [_], (_, p=[]) => p, // Drop the odd one out.
-    [], (a, p=[]) => p
+    _, (_, p=[]) => p,
   )
 
   t.deepEquals(toPairs([1]), [])
